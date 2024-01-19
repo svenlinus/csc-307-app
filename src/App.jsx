d2bc54f6613a04ffbe902e09bae4cb1cfc796830
linustoernqvist <linus.tornqvist@triggerbee.com>
2024-01-18 16:20:57 -0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+// src/MyApp.js
+import React, { useState } from "react";
+import Table from "./Table";
+
+function MyApp() {
+  const [characters, setCharacters] = useState([
+    {
+      name: "Charlie",
+      job: "Janitor" // the rest of the data
+    }
+  ]);
+
+  function removeOneCharacter(index) {
+    const updated = characters.filter((character, i) => {
+      return i !== index;
+    });
+    setCharacters(updated);
+  }
+  
+  return (
+    <div className="container">
+      <Table
+        characterData={characters}
+        removeCharacter={removeOneCharacter}
+      />
+    </div>
+  );
+}
+
+export default MyApp;
